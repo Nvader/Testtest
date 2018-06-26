117fdde06eb9961853d702db80f5bf9e2c922c3a
oleg.bugayev <oleg.bugayev@globallogic.com>
2018-06-26 15:41:47 +0300

    First task of Home Work with wide functionality

---
+++ src/com/company/Task3.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        int e = 5;
+
+        System.out.println(a + "\n" + b + "\n" + c + "\n" + d + "\n" + e);
+
+
+
+    }
+
+}
