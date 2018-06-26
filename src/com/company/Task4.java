5f60a428937218c2a8b0e0030610f9b1786221e9
oleg.bugayev <oleg.bugayev@globallogic.com>
2018-06-26 16:41:16 +0300

    Fourth task of Home Work - password

--- src/com/company/Task4.java
+++ src/com/company/Task4.java
@@ -4,17 +4,20 @@ import java.util.Scanner;
 
 public class Task4 {
     public static void main(String[] args) {
+
         System.out.println("Please enter your password below: ");
 
-        String parol = new Scanner(System.in).next();
+        int password = Integer.parseInt(new Scanner(System.in).next());
 
-        if (parol == "Oleg") {
-            System.out.println("You entered this password: " + parol + " this is correct password!");
+        if (password == 1234) {
+            System.out.println("You entered this password: " + password + " this is correct password!");
         } else {
 
-            System.out.println("You entered this password: " + parol + " this is  incorrect password, try again.");
+            System.out.println("You entered this password: " + password + " this is incorrect password, try again.");
 
 
         }
+
+
     }
 }
