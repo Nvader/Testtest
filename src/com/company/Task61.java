209ef151756e544628a76b3df43887cba344da23
oleg.bugayev <oleg.bugayev@globallogic.com>
2018-07-02 17:02:08 +0300

    Fourth task of Home Work - password

---
+++ src/com/company/Task61.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Task61 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 101; i++) {
+            if (i % 2 == 0){
+                System.out.println("These are even digits: " + i);
+
+            }
+
+        }
+    }
+}
