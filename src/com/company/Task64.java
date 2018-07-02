54bb4876452c8ad9c2237b9f6d57418a87a552d4
oleg.bugayev <oleg.bugayev@globallogic.com>
2018-07-02 17:14:47 +0300

    Sixth task of Home Work - digits dwvision on 5 and 7

---
+++ src/com/company/Task64.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Task64 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 101; i++) {
+            if (i % 5 == 0 && i % 7 == 0){
+                System.out.println("These are digits that could be devided onto 5 and 7: " + i);
+
+            }
+
+        }
+    }
+}
