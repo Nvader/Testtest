0d98a6adfa0b86c4523013a7d098a096719204ce
oleg.bugayev <oleg.bugayev@globallogic.com>
2018-07-02 17:10:57 +0300

    Sixth task of Home Work - digits dwvision on 3 and 9

---
+++ src/com/company/Task63.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Task63 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 101; i++) {
+            if (i % 3 == 0 || i % 9 == 0) {
+                System.out.println("These are digits that could be devided onto 3 or 9: " + i);
+
+//            } else if (i % 9 == 0){
+//                System.out.println("These are digits that could be devided onto 3: " + i);
+//            }
+
+
+            }
+        }
+    }
+}
